8c80f82cb9721e648bf0074e370b8314e4e8e020
Daniel Liu <daniel.liu1973@gmail.com>
2016-12-14 16:20:22 -0500

    ...

--- hw1/compare_es6.js
+++ hw1/compare_es6.js
@@ -1,8 +1,9 @@
 module.exports = compare = (oldData, newData) => {
   const result = { added: [], deleted: [], modified: [] };
 
-  oldData.map(o => (c => (c.length === 0 ? result.deleted.push(o) :
-      JSON.stringify(c[0])!== JSON.stringify(o.ext) ? result.modified.push(c[0]) : '')
+  oldData.map(o => (c => (
+    c.length === 0 ? result.deleted.push(o) : 
+    JSON.stringify(c[0])!== JSON.stringify(o.ext) ? result.modified.push(c[0]) : '')
     )(newData.filter(n => (o.firstName === n.firstName && o.lastName === n.lastName)))
   );
 
