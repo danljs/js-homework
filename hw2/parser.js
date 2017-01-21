07a01e67caaf7d2d512c64f5594611aaec8af73b
danljs <daniel.liu1973@gmail.com>
2017-01-21 12:11:35 -0500

    again

--- hw2/parser.js
+++ hw2/parser.js
@@ -1,26 +1,24 @@
 const http = require('http');
 const jsdom = require('jsdom').jsdom;
 
-module.exports = url => new Promise(resolve => {
-  http.get(url, response => {
-    let html = '';
-    response.on('data', chunk => html += chunk);
+module.exports = url => new Promise(resolve => http.get(url, res => {
+  let html = '';
+  res.on('data', chunk => html += chunk);
 
-    response.on('end', () => {
-      const doc = jsdom(html);
-      const keys = doc.querySelectorAll('tr th');
-      const values = doc.querySelectorAll('tr td');
-      let json = '[';
-      for (let i = 0; i < values.length; i * keys.length) {
-        json += '{';
-        for (let j = 0; j < keys.length; j++, i++) {
-          json += `"${keys[j].textContent}":"${values[i].textContent}",`;
-        }
-        json = `${json.slice(0, -1)}},`;
+  res.on('end', () => {
+    const doc = jsdom(html);
+    const keys = doc.querySelectorAll('tr th');
+    const values = doc.querySelectorAll('tr td');
+    let json = '[';
+    for (let i = 0; i < values.length; i * keys.length) {
+      json += '{';
+      for (let j = 0; j < keys.length; j++, i++) {
+        json += `"${keys[j].textContent}":"${values[i].textContent}",`;
       }
-      json = `${json.slice(0, -1)}]`;
+      json = `${json.slice(0, -1)}},`;
+    }
+    json = `${json.slice(0, -1)}]`;
 
-      resolve(json);
-    });
+    resolve(json);
   });
-});
+}));
