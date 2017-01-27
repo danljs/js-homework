d700cb1c928810a16f368d6485656b1017d1b592
danljs <daniel.liu1973@gmail.com>
2017-01-26 23:38:13 -0500

    a little bit better.

--- hw3/test.js
+++ hw3/test.js
@@ -1,18 +1,14 @@
 const http = require('http');
 const jsdom = require('jsdom').jsdom;
 const fs = require('fs');
+const path = require('path');
 
 ((url, keys, key, last) => {
-  const data_dir = __dirname + '/data';
-  !!!fs.existsSync(data_dir) ? fs.mkdirSync(data_dir) : '';
-
-  const today = new Date().toISOString().slice(0, 10);
-
-  const parser = url => new Promise(resolve => http.get(url, res => {
+  const parser = () => new Promise(resolve => http.get(url, res => {
     let html = '';
-    res.on('data', chunk => html += chunk);
-
-    res.on('end', () => {
+    res
+    .on('data', chunk => html += chunk)
+    .on('end', () => {
       const doc = jsdom(html);
       const names = doc.querySelectorAll('tr th');
       const values = doc.querySelectorAll('tr td');
@@ -33,12 +29,12 @@ const fs = require('fs');
     const oldDataObj = {};
 
     const isChanged = (o, n) => {
-      for (let i = 0; i < keys.length; i++){
+      for (let i = 0; i < keys.length; i++) {
         if (o[keys[i]] !== n[keys[i]]) {
-          return true ;
+          return true;
         }
       }
-      return false ;
+      return false;
     };
 
     oldData.map(o => oldDataObj[o[key]] = o);
@@ -58,33 +54,34 @@ const fs = require('fs');
     return result;
   }
 
-  parser(url).then(
-    e1 => {
-      fs.writeFile(`${data_dir}/${today}.json`, JSON.stringify(e1), 'utf8', err => {
-        if (err) {
-          return console.log(err);
-        }
+  const data_dir = path.join(__dirname, '/data');
+  !fs.existsSync(data_dir) ? fs.mkdirSync(data_dir) : '';
 
-        const oldData = `${data_dir}/${last}.json`;
-        fs.exists(oldData, exists => {
-          if (exists) {
-            fs.readFile(oldData, (err, contents) => {
-              if (err) {
-                return console.log(err);
-              }
-              console.log(compare(JSON.parse(contents, 'utf8'), e1))
-            });
-          } else {
-            console.log('Nothing Changed!');
-          }
-        });
-      });
-    }
-  );
+  const today = new Date().toISOString().slice(0, 10);
+
+  const output = e1 => {
+    const oldData = `${data_dir}/${last}.json`;
+    fs.exists(oldData, exists => {
+      if (!exists) {
+        return console.log('Nothing Changed!');
+      }
+
+      let contents = '';
+      fs.createReadStream(oldData)
+      .on('data', chunk => contents += chunk)
+      .on('end', () => console.log(compare(JSON.parse(contents, 'utf8'), e1)));
+    });
+  };
+
+  parser().then(e1 => {
+    const wstream = fs.createWriteStream(`${data_dir}/${today}.json`);
+    wstream.on('finish', () => output(e1));
+    wstream.write(JSON.stringify(e1))
+    wstream.end();
+  });
 })(
   'http://web-aaronding.rhcloud.com/employee.html',
   ['First Name', 'Last Name', 'Extension', 'Cell Number', 'Alternative NumberEmergency Only', 'Title'],
   'E-mail Address',
   '2017-01-25'
 );
-
