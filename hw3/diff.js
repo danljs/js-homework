61ebc1b17f5bb7bdb510c66eec195dcc5df25bec
danljs <daniel.liu1973@gmail.com>
2017-01-31 22:54:41 -0500

    ref

--- hw3/diff.js
+++ hw3/diff.js
@@ -9,90 +9,95 @@ const key = 'E-mail Address';
 const data_dir = path.join(__dirname, '/data');
 const today = new Date().toISOString().slice(0, 10);
 
-module.exports = function (last) {
-  function isChanged(o, n) {
-    for (let i = 0; i < keys.length; i++) {
-      if (o[keys[i]] !== n[keys[i]]) return true;
-    }
-    return false;
-  }
+function Diff(last) {
+  this.last = last;
+}
 
-  function compare(oldData, newData) {
-    const result = { added: [], deleted: [], modified: [] };
-    const oldDataObj = {};
-    oldData.map(o => oldDataObj[o[key]] = o);
-    newData.map(n => {
-      const o = oldDataObj[n[key]];
-      if (!o) {
-        result.added.push(n);
-      } else {
-        if (isChanged(o, n)) result.modified.push({ before: o, after: n });
-        delete oldDataObj[n[key]];
+Diff.prototype.isChanged = function (o, n) {
+  for (let i = 0; i < keys.length; i++) {
+    if (o[keys[i]] !== n[keys[i]]) return true;
+  }
+  return false;
+}
+
+Diff.prototype.init = function () {
+  return new Promise(resolve => {
+    fs.stat(data_dir, err => {
+      if (!!err && err.code === 'ENOENT') {
+        fs.mkdir(data_dir, err1 => {
+          if (err1) return console.error(err1);
+        });
       }
+      resolve();
     });
-    result.deleted.push(oldDataObj);
-    return result;
-  }
+  });
+}
 
-  function init() {
-    return new Promise(resolve => {
-      fs.stat(data_dir, err => {
-        if (!!err && err.code === 'ENOENT') {
-          fs.mkdir(data_dir, err1 => {
-            if (err1) return console.error(err1);
-          });
+Diff.prototype.compare = function (oldData, newData) {
+  const result = { added: [], deleted: [], modified: [] };
+  const oldDataObj = {};
+  oldData.map(o => oldDataObj[o[key]] = o);
+  newData.map(n => {
+    const o = oldDataObj[n[key]];
+    if (!o) {
+      result.added.push(n);
+    } else {
+      if (this.isChanged(o, n)) result.modified.push({ before: o, after: n });
+      delete oldDataObj[n[key]];
+    }
+  });
+  result.deleted.push(oldDataObj);
+  return result;
+}
+
+Diff.prototype.fetch = function () {
+  return new Promise(resolve => http.get(url, res => {
+    let html = '';
+    res
+    .on('data', chunk => html += chunk)
+    .on('end', () => {
+      const doc = jsdom(html);
+      const names = doc.querySelectorAll('tr th');
+      const values = doc.querySelectorAll('tr td');
+      const rows = [];
+      for (let i = 0; i < values.length; i += names.length) {
+        const row = {};
+        for (let j = 0; j < names.length; j += 1) {
+          row[names[j].textContent] = values[i + j].textContent;
         }
-        resolve();
-      });
+        rows.push(row);
+      }
+      resolve(rows);
     });
-  }
+  }));
+}
 
-  function fetch() {
-    return new Promise(resolve => http.get(url, res => {
-      let html = '';
-      res
-      .on('data', chunk => html += chunk)
-      .on('end', () => {
-        const doc = jsdom(html);
-        const names = doc.querySelectorAll('tr th');
-        const values = doc.querySelectorAll('tr td');
-        const rows = [];
-        for (let i = 0; i < values.length; i += names.length) {
-          const row = {};
-          for (let j = 0; j < names.length; j += 1) {
-            row[names[j].textContent] = values[i + j].textContent;
-          }
-          rows.push(row);
-        }
-        resolve(rows);
-      });
-    }));
-  }
-
-  function save(data) {
-    return new Promise(resolve => {
-      fs.writeFile(`${data_dir}/${today}.json`, JSON.stringify(data), err => {
-        if (err) return console.log(err);
-        resolve(data);
-      });
-    });
-  }
-
-  function output(newData) {
-    fs.readFile(`${data_dir}/${last}.json`, (err, oldData) => {
+Diff.prototype.save = function (data) {
+  return new Promise(resolve => {
+    fs.writeFile(`${data_dir}/${today}.json`, JSON.stringify(data), err => {
       if (err) return console.log(err);
-      const result = compare(JSON.parse(oldData, 'utf8'), newData);
-      console.log('---added---');
-      console.log(result.added);
-      console.log('---deleted---');
-      console.log(result.deleted);
-      console.log('---modified---');
-      console.log(result.modified);
+      resolve(data);
     });
-  }
+  });
+}
 
-  init()
-  .then(() => fetch())
-  .then(data => save(data))
-  .then(data => output(data));
+Diff.prototype.output = function (newData) {
+  fs.readFile(`${data_dir}/${this.last}.json`, (err, oldData) => {
+    if (err) return console.log(err);
+    const result = this.compare(JSON.parse(oldData, 'utf8'), newData);
+    console.log('---added---');
+    console.log(result.added);
+    console.log('---deleted---');
+    console.log(result.deleted);
+    console.log('---modified---');
+    console.log(result.modified);
+  });
+}
+
+module.exports = function (last) {
+  const d = new Diff(last);
+  d.init()
+  .then(() => d.fetch())
+  .then(data => d.save(data))
+  .then(data => d.output(data));
 }
